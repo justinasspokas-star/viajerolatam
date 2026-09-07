b172c09332e9f60523d6a2d50c4caac86feaab37
justinasspokas-star <justinasspokas@gmail.com>
2026-09-07 12:31:18 +0200

    Add files via upload

---
+++ stripe-config.js
@@ -0,0 +1,6 @@
+// ViajeroLatam Stripe Payment Links
+// Enlaces LIVE configurados para ViajeroLatam.
+window.VL_STRIPE = {
+  flightPaymentLink: 'https://buy.stripe.com/5kQ28q2CZ3Dhc4o8Bw7ss01',
+  accommodationPaymentLink: 'https://buy.stripe.com/3cIbJ0gtPc9N2tOg3Y7ss00'
+};
